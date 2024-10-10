3463a1b84872ceabc828683dd09d66665758cd57
nithishp <nithishj03@gmail.com>
2024-10-10 11:14:34 +0530

    added youtbe embed

--- components/SpringModal.jsx
+++ components/SpringModal.jsx
@@ -53,7 +53,7 @@ const SpringModal = ({ isOpen, setIsOpen }) => {
                 {/* 16:9 aspect ratio */}
                 <iframe
                   className="absolute top-0 left-0 w-full h-full rounded-2xl"
-                  src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+                  src="https://www.youtube.com/embed/-rL-HOqHhfE?si=czZuqm5eZqSoHnwr"
                   title="YouTube video"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
